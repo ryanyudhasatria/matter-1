c97d067287aadf811b6443bdbc790bbc85e4dd58
ryanyudhasatria <ryansatria@hacktiv8.com>
2019-01-22 16:12:15 +0700

    fixing teacher card

--- src/molecules/TeacherCard/TeacherCard.stories.js
+++ src/molecules/TeacherCard/TeacherCard.stories.js
@@ -19,13 +19,27 @@ storiesOf('Molecule.TeacherCard', module)
         (() => (
             <div>
                 <TeacherCardMolecule 
-                    typeCard={text('Type', 'GREY')}
-                    type={text('Type', 'REGULAR_ROUNDED')}
                     src='http://infinitydream.net/adriatic/wp-content/uploads/2017/01/avatar03.jpg'
                     alt='john'
                     name="Tom T"
                     instructorRole="Curriculum Director at Hacktiv8"
-                    description="The course is quite valuable, as it is rigorous and provides me an opportunity to learn and practice applying cutting edge skills. The personalized feedback provided on the submitted projects is encouraging and provides me insight in areas where I find challenging."
+                    description="I am a veteran mobile developer having built over 57 mobile apps for iOS, and I've also build multiple Unity 3D games, including Call of Duty Ghosts mobile.."
+                />
+
+                <TeacherCardMolecule 
+                    src='ImageNotFound'
+                    alt='john'
+                    name="Tom T"
+                    instructorRole="Curriculum Director at Hacktiv8"
+                    description="I am a veteran mobile developer having built over 57 mobile apps for iOS, and I've also build multiple Unity 3D games, including Call of Duty Ghosts mobile.."
+                /> 
+
+                <TeacherCardMolecule 
+                    src=''
+                    alt='john'
+                    name="Tom T"
+                    instructorRole="Curriculum Director at Hacktiv8"
+                    description="I am a veteran mobile developer having built over 57 mobile apps for iOS, and I've also build multiple Unity 3D games, including Call of Duty Ghosts mobile.."
                 />                
             </div>                    
     )));
