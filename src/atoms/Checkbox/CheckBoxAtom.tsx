e2ce0ebddd0b284c5d48fd8e0178bf6700851cbe
ryanyudhasatria <ryansatria@hacktiv8.com>
2019-01-25 18:07:24 +0700

    add checkbox

--- src/atoms/Checkbox/CheckBoxAtom.tsx
+++ src/atoms/Checkbox/CheckBoxAtom.tsx
@@ -1,6 +1,92 @@
 import * as React from 'react';
-import { cx, css } from 'emotion';
-import * as style from './CheckBoxAtom.css';
+import {  css } from 'emotion';
+
+export const checkmark = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 24px;
+  width: 24px;
+  background-color: #B8C2CC;
+  border-radius: 100%;
+
+  &:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    display: block;
+  }
+`
+
+export const container = css`
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 14px;
+  padding-top: 5px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  & input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  &:hover input ~ .${checkmark} {
+    background-color: #ccc;  
+  }
+
+  & input:checked ~ .${checkmark} {
+    background-color: #fff;
+    border: 1px solid #1BCBD1;
+  }
+
+  & input:checked ~ .${checkmark}:after {
+    display: block;
+  }
+
+  & input:checked ~ .${checkmark}:before {
+    display: block;
+  }
+
+  & .${checkmark}:after {
+    left: 8px;
+    top: 4px;
+    width: 5px;
+    height: 10px;
+    border: solid #1BCBD1;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+  & .${checkmark}:before {
+    left: 8px;
+    top: 4px;
+    width: 5px;
+    height: 10px;
+    border: solid #fff;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+`
+
+//@ts-ignore
+import COLORS from '../../constants/colors';
 
 interface IProps {
     className?: any,
@@ -10,21 +96,11 @@ interface IProps {
     value?:string,
     htmlFor?:string,
     description?:string,
-}
-
-// const container = {
-//     display: 'block',
-//     position: 'relative',
-//     paddingLeft: '35px',
-//     marginBottom: '12px',
-//     cursor: 'pointer',
-//     fontSize: '22px',
-//     userSelect: 'none',
-// }
-            
-        
+}     
 
 class CheckBoxAtom extends React.PureComponent<IProps, any>{
+
+
     state = {}
 
     render(){
@@ -33,12 +109,13 @@ class CheckBoxAtom extends React.PureComponent<IProps, any>{
             value = '',
             htmlFor = '',
             description = '',
+            
         } = this.props;
          
         return (            
-            <label className={style.container}>
+            <label className={container}>{description}
                 <input type="checkbox" id={id} value={value}/>
-                <label htmlFor={htmlFor} className={cx('', css``)}>{description}</label>
+                <label htmlFor={htmlFor} className={checkmark}></label>
             </label>
         )
     }
