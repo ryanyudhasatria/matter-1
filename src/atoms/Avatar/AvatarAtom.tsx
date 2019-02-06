37b6bd859ee5bab96ce052dcec3817414479150c
ryanyudhasatria <ryansatria@hacktiv8.com>
2019-02-06 15:47:59 +0700

    fix avatar no image,  names trim undefined

--- src/atoms/Avatar/AvatarAtom.tsx
+++ src/atoms/Avatar/AvatarAtom.tsx
@@ -12,9 +12,16 @@ const avatarRounded = 'border-radius: .25rem;';
 const generateInitial = (name = 'User') => {
   const names = name.trim().split(' ');
   if (names.length === 1) {
-    return name[0];
-  } else {
-    return names[0][0] + (names[1][0] ? names[1][0] : '');    
+    console.log('A')
+    return name[0];    
+  }
+  else if (names[0][0] != null && names[1][0] != null ) {
+    console.log('B')
+    return names[0][0] + ((names[1][0] != null )? names[1][0] : '');    
+  }
+  else {
+    console.log('C')
+    return names[0][0] + ((names[1][0] == null )? names[2][0] : '');    
   }
 };
 
