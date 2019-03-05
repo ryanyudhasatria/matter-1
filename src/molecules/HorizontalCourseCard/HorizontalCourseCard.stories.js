51f4af24935445eb47fff4082e19f1e9493fe110
ryanyudhasatria <ryansatria@hacktiv8.com>
2019-03-05 18:26:49 +0700

    add duration & rating on CourseCardHorizontal & CourseCard (for catalog page), & add bookmark component

--- src/molecules/HorizontalCourseCard/HorizontalCourseCard.stories.js
+++ src/molecules/HorizontalCourseCard/HorizontalCourseCard.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import COLORS from '../../constants/colors'
-import {cx, css } from 'emotion'
-
+import COLORS from '../../constants/colors';
+import {cx, css } from 'emotion';
+import { ButtonAtom } from '../../atoms/Button/ButtonAtom';
 import styles from '../../styles';
 import { HorizontalCourseCardMolecule } from './HorizontalCourseCardMolecule';
 
@@ -20,7 +20,7 @@ storiesOf('Molecule.HorizontalCourseCardMolecule', module)
     <div className="flex flex-column pa4 w-100 ba b--silver">
       <HorizontalCourseCardMolecule
         small
-        className="mv3 w-80"
+        className="mv3"
         title="High Impact Presentation"
         level={{ name: 'Intermediate', label: 'Intermediate '}}
         instructor="Roma Tampubolon"
@@ -29,56 +29,59 @@ storiesOf('Molecule.HorizontalCourseCardMolecule', module)
         rating="5"
         reviewCounts="32"
         instructorRole="Curriculum Director at Hacktiv8"
-        ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lebih lanjut</b></a> }
+        // ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lihat Course Ini</b></a> }
+        ActionButtonComponent={() => <ButtonAtom type="SMALL_PRIMARY">Lihat Course Ini</ButtonAtom>  }
         coverImage="https://s3-ap-southeast-1.amazonaws.com/kode-images/public/course-covers/es6.png"
         description="Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi."
-      />
-      <HorizontalCourseCardMolecule
-        small
-        className="mv3"
-        title="Team Engagement: Dimulai dari Hal-Hal Kecil"
-        level={{ name: 'Beginner', label: 'Beginner '}}
-        instructor=""
-        category="Development"
-        instructorRole="Curriculum Director at Hacktiv8"
-        collaborator="Kompas"
-        duration=""
-        rating="4.2"
-        reviewCounts="22"
-        coverImage=""        
-        description="Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi."
-      />
-      <HorizontalCourseCardMolecule
-        small
-        className="mv3"
-        title="High Impact Presentation"
-        level={{ name: 'Intermediate', label: 'Intermediate '}}
-        instructor="Roma Tampubolon"
-        category="Development"
-        instructorRole="Curriculum Director at Hacktiv8"
-        collaborator=""
-        duration="10"
-        rating=""
-        reviewCounts="32"
-        ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lebih lanjut</b></a> }
-        coverImage="https://s3-ap-southeast-1.amazonaws.com/kode-images/public/course-covers/es6.png"
-        description=""
-      />
-      <HorizontalCourseCardMolecule
-        small
-        className="mv3"
-        title="High Impact Presentation"
-        level={{ name: 'Intermediate', label: 'Intermediate '}}
-        instructor="Roma Tampubolon"
-        category="Development"
-        instructorRole="Curriculum Director at Hacktiv8"
-        collaborator=""
-        duration="10"
-        rating=""
-        reviewCounts="32"
-        ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lebih lanjut</b></a> }
-        coverImage="https://s3-ap-southeast-1.amazonaws.com/kode-images/public/course-covers/redis.png"
-        description="Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi. Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi."
-      />
+      />       
     </div>
   )));
+
+
+      // <HorizontalCourseCardMolecule
+      //   small
+      //   className="mv3"
+      //   title="Team Engagement: Dimulai dari Hal-Hal Kecil"
+      //   level={{ name: 'Beginner', label: 'Beginner '}}
+      //   instructor=""
+      //   category="Development"
+      //   instructorRole="Curriculum Director at Hacktiv8"
+      //   collaborator="Kompas"
+      //   duration=""
+      //   rating="4.2"
+      //   reviewCounts="22"
+      //   coverImage=""        
+      //   description="Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi."
+      // />
+      // <HorizontalCourseCardMolecule
+      //   small
+      //   className="mv3"
+      //   title="High Impact Presentation"
+      //   level={{ name: 'Intermediate', label: 'Intermediate '}}
+      //   instructor="Roma Tampubolon"
+      //   category="Development"
+      //   instructorRole="Curriculum Director at Hacktiv8"
+      //   collaborator=""
+      //   duration="10"
+      //   rating=""
+      //   reviewCounts="32"
+      //   ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lebih lanjut</b></a> }
+      //   coverImage="https://s3-ap-southeast-1.amazonaws.com/kode-images/public/course-covers/es6.png"
+      //   description=""
+      // />
+      // <HorizontalCourseCardMolecule
+      //   small
+      //   className="mv3"
+      //   title="High Impact Presentation"
+      //   level={{ name: 'Intermediate', label: 'Intermediate '}}
+      //   instructor="Roma Tampubolon"
+      //   category="Development"
+      //   instructorRole="Curriculum Director at Hacktiv8"
+      //   collaborator=""
+      //   duration="10"
+      //   rating=""
+      //   reviewCounts="32"
+      //   ActionButtonComponent={() => <a className={cx('f6 link dim br-pill ph3 pv2 dib white', css`background: ${COLORS.GREY.LIGHT}; color: ${COLORS.BLACK.NORMAL}`)} href="#0"><b>Lebih lanjut</b></a> }
+      //   coverImage="https://s3-ap-southeast-1.amazonaws.com/kode-images/public/course-covers/redis.png"
+      //   description="Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi. Keterlibatan adalah alasan kenapa sebuah tim bisa berhasil. Saat ini, banyak organisasi mengalami tantangan dalam mengelola dan meningkatkan produktivitas di tempat kerja. Di course ini, team leader bisa belajar bagaimana menciptakan lingkungan kerja yang baik dengan cara mengintegrasikan prinsip-prinsip keterlibatan karyawan dan secara positif meningkatkan keterlibatan dan kinerja organisasi."
+      // />
